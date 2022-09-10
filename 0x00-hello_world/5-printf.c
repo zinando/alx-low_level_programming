d76998f757c6ae1b95e2e3d7f788547acf7e81d4
zinando <belovedsamex@yahoo.com>
2022-09-09 23:39:39 -0700

    Write a c programe that prints with the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * Description - print 'with proper grammar, but the outcome is a piece of art,' using the printf function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void){
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
