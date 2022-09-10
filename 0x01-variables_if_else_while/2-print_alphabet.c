7f863411c4b14965fb70e9509e824de4d500bd75
zinando <belovedsamex@yahoo.com>
2022-09-10 12:55:13 -0700

    Print alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * description - function that prints the value of a variabl
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+	char ch;
+		
+	for (x = 97; x < 123; x++)
+	{	
+		ch = x;
+		putchar(ch);
+		putchar('\n');
+	}	
+	return (0);
+}
