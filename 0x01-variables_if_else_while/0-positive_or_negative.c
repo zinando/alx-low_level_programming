e8e8374411417f57e639242ed186a9cbec5a067f
zinando <belovedsamex@yahoo.com>
2022-09-10 09:17:35 -0700

    Write an if statement in c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,14 +10,14 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n>0)
+	if (n > 0)
 	{
 		printf("%d is positive\n", n);
-	} else if (n<0)
+	} else if (n < 0)
 	{
 		printf("%d is negative\n", n);
 	} else{
-		printf("%d is zero", n);
+		printf("%d is zero\n", n);
 	}
 	return (0);
 }
