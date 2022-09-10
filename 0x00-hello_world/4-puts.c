a410ee1bbe83f1d3b670c63fa4c9e1914e86f54f
zinando <belovedsamex@yahoo.com>
2022-09-09 23:19:08 -0700

    Write a c programe that prints text

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- *  * main - Entry point
- *   *
- *    * Description: Programming is like building a multilingual puzzle
- *     *
- *      * Return: Always 0 (Success)
- *       */
+ * main - Entry point
+ * 
+ * Description: Programming is like building a multilingual puzzle
+ * 
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 		char Mystr[] = "\"Programming is like building a multilingual puzzle";
