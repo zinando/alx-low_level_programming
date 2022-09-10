ae4c3e443a5c370a22e48e2733597429f2375596
zinando <belovedsamex@yahoo.com>
2022-09-09 23:23:00 -0700

    Write a c programme that prints text

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 /**
  * main - Entry point
- * 
+ *
  * Description: Programming is like building a multilingual puzzle
- * 
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
