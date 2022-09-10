d7967fe82e6434a3cfab5665994a2fc80260dedd
zinando <belovedsamex@yahoo.com>
2022-09-10 14:11:02 -0700

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * description - function that prints the value of a variabl
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+
+	for (x = 0; x < 10; x++)
+	{
+		printf("%d", x);
+	}
+	putchar('\n');
+	return (0);
+}
