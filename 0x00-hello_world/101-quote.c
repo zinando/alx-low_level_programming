700189cfaebd3e966a8535b7d07f746db1e1ee21
zinando <belovedsamex@yahoo.com>
2022-09-10 02:51:39 -0700

    Fix it

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include <unistd.h>
 /**
  * main - entry point
  *
@@ -6,6 +7,6 @@
  */
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58, 1, stderr);
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }	
