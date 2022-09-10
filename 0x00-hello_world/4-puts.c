96ef6935d284ad16e30c281e74addeb3ad072d5d
zinando <belovedsamex@yahoo.com>
2022-09-09 22:37:37 -0700

    Write a c programe

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+x#include<stdio.h>
+/**
+ *  * main - Entry point
+ *   *
+ *    * Description: Programming is like building a multilingual puzzle
+ *     *
+ *      * Return: Always 0 (Success)
+ *       */
+int main(void)
+{
+		char Mystr[] = "\"Programming is like building a multilingual puzzle";
+
+			puts(Mystr);
+
+				return (0);
+}
