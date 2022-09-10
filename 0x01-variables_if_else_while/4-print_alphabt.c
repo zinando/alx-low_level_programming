008460575beb470087cb7b5262a8f683525e0f43
zinando <belovedsamex@yahoo.com>
2022-09-10 13:52:17 -0700

    Print and ommit letters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h5>
+/**
+ * main - entry point
+ *
+ * description - function that prints the value of a variabl
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+	char ch;
+
+	for (x = 97; x < 123; x++)
+	{
+		if (x != 101 || x != 113)
+		{
+			ch = x;
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
