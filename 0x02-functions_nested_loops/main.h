ee1cc0776bbe4fa203b7217c43414b3284468b61
zinando <belovedsamex@yahoo.com>
2022-09-16 10:26:16 -0700

    Create header file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,4 +4,13 @@ int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+int _isalpha(int c);
+void print_times_table(int n);
 #endif
