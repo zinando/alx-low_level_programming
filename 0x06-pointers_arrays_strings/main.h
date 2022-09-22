8416e4a63301c4be2471a7a3c98e9d626f3f3b6d
zinando <belovedsamex@yahoo.com>
2022-09-21 20:08:26 -0700

    Print numbers

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char ch);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -12,5 +11,6 @@ char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
+int _putchar(char c);
 
 #endif
