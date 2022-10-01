84edfa3d5029b9ecf4c433e6548eae8d9c58853c
zinando <belovedsamex@yahoo.com>
2022-10-01 04:53:24 -0700

    Print the sum of numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - entry point. Prints the sum of two numbers
+ * passed as arguments
+ * or Prints Error if two arguments were not provided
+ *
+ * @argc: number of command line args
+ * @argv: array of command ine args
+ *
+ * Return: Success 0 or Error 1
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 3)
+	{	int i;
+		int j;
+		int sum;
+
+		i = atoi(argv[argc - 1]);
+		j = atoi(argv[argc - 2]);
+		sum = i + j;
+
+		printf("%d\n", sum);
+
+		return (0);
+	}
+	printf("Error\n");
+
+	return (1);
+}
