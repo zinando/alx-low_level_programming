bf3d59468e022a078856032bf5e0016a7e069342
zinando <belovedsamex@yahoo.com>
2022-10-01 03:36:46 -0700

    Surpress unused arg

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - entry point. Prints number of args passed into it
+ * @argc: number of command line args
+ * @argv: array of command ine args
+ *
+ * Return: Success
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
