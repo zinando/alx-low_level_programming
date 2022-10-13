6dd0d6e4709a831edfc10236c8189534f6854cfe
zinando <belovedsamex@yahoo.com>
2022-10-13 09:35:32 -0700

    Understand the preprocessing process

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+	puts(__FILE__);
+
+	return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+This is where i learn about the compilation process in c programming
